2c3fcdddff58470d696346068014dac1f015c41f
TomaszKotowski <tomaszk72@gmail.com>
2017-10-08 13:52:35 +0200

    Finished first part
    The clock works but there is a clunky transition at the end of every minute. The hands roatate completly insed of few degrees forwards.

---
+++ 2/app.js
@@ -0,0 +1,23 @@
+    const secondHand = document.querySelector('.second-hand');
+    const minsHand = document.querySelector('.minute-hand');
+    const hourHand = document.querySelector('.hour-hand');
+
+
+    function setDate() {
+        const now = new Date();
+
+        const seconds = now.getSeconds();
+        const secondsDegrees = ((seconds / 60) * 360) + 90;
+        secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+        const minutes = now.getMinutes();
+        const minutesDegrees = ((minutes / 60) * 360) + 90;
+        minsHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+        const hours = now.getHours();
+        const hoursDegrees = ((hours / 12) * 360) + 90;
+        hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
+    }
+
+    setInterval(setDate, 1000);
