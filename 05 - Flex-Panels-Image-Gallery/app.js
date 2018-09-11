910b39bb110a1a6b7e3f7c3a29863b75fe0f74ba
TomaszKotowski <tomaszk72@gmail.com>
2018-09-11 13:58:47 +0200

    done exercise 8

--- 05 - Flex-Panels-Image-Gallery/app.js
+++ 05 - Flex-Panels-Image-Gallery/app.js
@@ -5,3 +5,18 @@ document.querySelector('.navigation__title').innerHTML = title;
 /**
  * Exercise JS
  */
+
+const panels = document.querySelectorAll('.panel');
+
+function toggleOpen() {
+  this.classList.toggle('open');
+}
+function toggleActive(event) {
+  console.log(event.propertyName);
+  if (event.propertyName.includes('flex')) {
+    this.classList.toggle('open-active');
+  }
+}
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
